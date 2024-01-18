4b6ca43caec01c9ee7d30de89c1146567c46e4f8
SaidOubaassine <saidoubaassine04@gmail.com>
2024-01-18 17:02:21 +0100

    test that student can be created and recalled

--- jdbc/src/test/java/learn/spring/jdbc/dao/impl/StudentDaoImplIntegrationTests.java
+++ jdbc/src/test/java/learn/spring/jdbc/dao/impl/StudentDaoImplIntegrationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,5 +30,29 @@ public class StudentDaoImplIntegrationTests {
         Optional<Student> result = underTest.findOne(student.getId());
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(student);
+    }
+    @Test
+    public void testThatMultipleStudentsCanBeCreatedAndRecalled(){
+        Student studentA = TestDataUtil.createTestStudentA();
+        underTest.create(studentA);
+        Student studentB = TestDataUtil.createTestStudentB();
+        underTest.create(studentB);
+        Student studentC = TestDataUtil.createTestStudentC();
+        underTest.create(studentC);
+
+        List<Student> result = underTest.find();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(studentA, studentB, studentC);
+    }
+    @Test
+    public void testThatStudentCanBeUpdated(){
+        Student studentA = TestDataUtil.createTestStudentA();
+        studentA.setName("Kamal");
+        underTest.create(studentA);
+        underTest.update(studentA.getId(), studentA);
+        Optional<Student> result = underTest.findOne(studentA.getId());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(studentA);
     }
 }
